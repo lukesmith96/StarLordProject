40ec3e6664c62d55617d2aec5fa6a962af83ba4d
Joshua King <JKing22@calpoly.edu>
2017-11-05 15:23:13 -0800

    Tractor Beam + Shield Power Up
    right click to activate tractor beam, currently only pulls shield powerup

---
+++ Assets/Scripts/ShieldPickup.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldPickup : MonoBehaviour {
+
+	public GameObject Shield;
+
+	public float thrust = 10f;
+
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("Player")) {
+			gameObject.SetActive (false);
+			GameObject newShield = Instantiate (Shield, new Vector3 (0, 0, 1), Quaternion.identity);
+			newShield.transform.localScale = new Vector3 (0.1f, 0.1f, 0.1f);
+			int count = 100;
+			while (count > 0) {
+				newShield.transform.localScale += new Vector3 (1f, 1f, 1f) * (Time.deltaTime);
+				count--;
+			}
+			count = 100;
+		}
+	}
+
+	void OnTriggerStay2D (Collider2D other){
+		if (other.gameObject.CompareTag ("Beam") && other.gameObject.GetComponent<Renderer> ().enabled == true) {
+			transform.up = (Vector3.zero - transform.position);
+			transform.position += transform.up * Time.deltaTime * thrust;
+		} else if (other.gameObject.GetComponent<Renderer> ().enabled == false && rb2d.velocity != Vector2.zero) {
+			rb2d.velocity -= new Vector2 (0.1f, 0.1f);
+		}
+	}
+}
