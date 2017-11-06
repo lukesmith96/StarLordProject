65fd7cabadd0918458f7f168b2bb0a3bff7b5c77
U-K_PC\Kenny <koshima@calpoly.edu>
2017-11-05 16:09:03 -0800

    Cleaned up automatic turret code

--- Assets/Scripts/AutoTurretController.cs
+++ Assets/Scripts/AutoTurretController.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AutoTurretController : TurretController {
-   public GameObject player;
-   public GameObject enemy;
    public bool isTouching;
 
    private SpriteRenderer sprite;
@@ -46,79 +44,7 @@ public class AutoTurretController : TurretController {
 
       if (isAttached) {
       
-         //target is closest enemy with line of sight
-         RaycastHit2D closestEnemy = new RaycastHit2D ();
-
-         bool gotTarget = false;
-         Vector3 startRay = transform.position;
-         List<GameObject> enemyList = dynamicPool.GetPoolList (enemy);
-         if (enemyList == null) {
-            return;
-         }
-         //http://answers.unity3d.com/questions/1042247/how-to-make-a-simple-line-of-sight-in-a-2d-top-dow.html
-         foreach (GameObject childPos in enemyList) {
-            if (!childPos.activeInHierarchy)
-               continue;
-         
-            //precompute our ray settings
-            Vector3 directionRay = (childPos.transform.position - startRay).normalized;
-            float distanceRay = maxRange;
-
-            //draw the ray in the editor
-            Debug.DrawRay (startRay, directionRay * distanceRay, Color.red);
-
-            //do the ray test
-            RaycastHit2D[] sightTestResults = Physics2D.RaycastAll (startRay, directionRay, distanceRay);
-
-
-            //now iterate over all results to work out what has happened
-            for (int i = 0; i < sightTestResults.Length; i++) {
-               RaycastHit2D sightTest = sightTestResults [i];
-
-               //check if this is the closest, but also check if the player is in the way
-               //sightTest.transform.tag != "Player" && 
-               if (sightTest.transform.tag != "Enemy")
-                  continue;
-
-               if (!gotTarget) {
-                  closestEnemy = sightTest;
-                  gotTarget = true;
-               } else {
-                  if (sightTest.distance < closestEnemy.distance) {
-                     closestEnemy = sightTest;
-                  }
-               }
-            }
-         }
-
-         if (gotTarget) {
-            if (closestEnemy.transform.tag == "Player") {
-               gotTarget = false;
-            }
-         }
-      
-         if (gotTarget) {
-            Vector2 target = closestEnemy.transform.position;
-            Vector2 turret = transform.position;
-         
-            /* From https://forum.unity.com/threads/leading-a-target.193445/ */
-            float distance = Vector2.Distance (turret, target);
-            float travelTime = distance / speed;
-            Vector2 newTarget = target + closestEnemy.rigidbody.velocity * travelTime;
-         
-            float distance2 = Vector2.Distance (turret, target + (target - newTarget) / 2f);
-            float travelTime2 = distance2 / speed;
-            Vector2 newTarget2 = target + closestEnemy.rigidbody.velocity * travelTime2;
-            Vector2 direction = newTarget - turret;
-         
-            direction.Normalize ();
-         
-            transform.up = direction;
-         
-            if (closestEnemy.distance <= maxRange && closestEnemy.distance >= minRange) {
-               FireBullet (direction);
-            }
-         }
+         AutoFire ();
 
       }
 
--- Assets/Scripts/OrbitingTurretController.cs
+++ Assets/Scripts/OrbitingTurretController.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OrbitingTurretController : TurretController {
-   public GameObject player;
-   public GameObject enemy;
-
    public float radius = 10;
    public int orbitalSpeed = 50;
 
@@ -31,78 +28,7 @@ public class OrbitingTurretController : TurretController {
 
       base.Update ();
 
-      //target is closest enemy with line of sight
-      RaycastHit2D closestEnemy = new RaycastHit2D();
-
-      bool gotTarget = false;
-      Vector3 startRay = transform.position;
-      List<GameObject> enemyList = dynamicPool.GetPoolList(enemy);
-      if (enemyList == null)
-      {
-         return;
-      }
-      //http://answers.unity3d.com/questions/1042247/how-to-make-a-simple-line-of-sight-in-a-2d-top-dow.html
-      foreach (GameObject childPos in enemyList) {
-         if (!childPos.activeInHierarchy) continue;
-
-         //precompute our ray settings
-         Vector3 directionRay = (childPos.transform.position - startRay).normalized;
-         float distanceRay = maxRange;
-
-         //draw the ray in the editor
-         Debug.DrawRay(startRay, directionRay * distanceRay, Color.red);
-
-         //do the ray test
-         RaycastHit2D[] sightTestResults = Physics2D.RaycastAll(startRay,  directionRay, distanceRay);
-
-         //now iterate over all results to work out what has happened
-         for(int i = 0; i < sightTestResults.Length; i++) {
-            RaycastHit2D sightTest = sightTestResults[i];
-
-            //check if this is the closest, but also check if the player is in the way
-            if (sightTest.transform.tag != "Player" && sightTest.transform.tag != "Enemy") continue;
-
-            if (!gotTarget) {
-               closestEnemy = sightTest;
-
-               gotTarget = true;
-            } else {
-               if (sightTest.distance < closestEnemy.distance) {
-                  closestEnemy = sightTest;
-               }
-            }
-         }
-      }
-
-      if (gotTarget) {
-         if (closestEnemy.transform.tag == "Player") {
-            gotTarget = false;
-         }
-      }
-
-      if (gotTarget) {
-         Vector2 target = closestEnemy.transform.position;
-         Vector2 turret = transform.position;
-
-         /* From https://forum.unity.com/threads/leading-a-target.193445/ */
-
-         float distance = Vector2.Distance (turret, target);
-         float travelTime = distance / speed;
-         Vector2 newTarget = target + closestEnemy.rigidbody.velocity * travelTime;
-
-         float distance2 = Vector2.Distance (turret, target + (target-newTarget) / 2f);
-         float travelTime2 = distance2 / speed;
-         Vector2 newTarget2 = target + closestEnemy.rigidbody.velocity * travelTime2;
-         Vector2 direction = newTarget - turret;
-
-         direction.Normalize();
-
-         transform.up = direction;
-
-         if (closestEnemy.distance <= maxRange && closestEnemy.distance >= minRange) {
-            FireBullet(direction);
-         }
-      }
+      AutoFire ();
 	}
 
    private Vector2 GetRandPoint (Vector2 origin, float radius, out float angle) {
